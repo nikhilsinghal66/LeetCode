de4603c7b8b5b0c890c59d922684b6a5129df9ca
nikhilsinghal66 <nikhil24384@iiitd.ac.in>
2025-09-23 10:53:12 +0000

    Code challenge submission for question: 392 Is Subsequence

---
+++ 392 Is Subsequence/392is-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int a=0;
+        for(int i=0;i<t.size();i++){
+            if(s[a]==t[i]){
+                a++;
+            }
+
+        }
+        if(a==s.size()){
+            return true;
+        }
+        return false;  
+    }
+};
