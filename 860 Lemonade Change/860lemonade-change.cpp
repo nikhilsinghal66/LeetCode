9731bfc7a7978feab750158b162fd9674b21137b
nikhilsinghal66 <nikhil24384@iiitd.ac.in>
2025-10-06 15:36:38 +0000

    Code challenge submission for question: 860 Lemonade Change

---
+++ 860 Lemonade Change/860lemonade-change.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five=0;
+        int ten=0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5){
+                five++;
+            }
+            else if(bills[i]==10){
+                if(five>=1){
+                    five--;
+                    ten++;
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                if(five>=1 && ten>=1){
+                    five=five-1;
+                    ten--;
+                }
+                else if(five>=3){
+                    five=five-3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
