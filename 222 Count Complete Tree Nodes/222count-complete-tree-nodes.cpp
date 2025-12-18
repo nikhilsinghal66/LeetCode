72ccac6aa6ceb7af1067fe4c8d621cfe1341c262
nikhilsinghal66 <nikhil24384@iiitd.ac.in>
2025-12-18 02:34:17 +0000

    Code challenge submission for question: 222 Count Complete Tree Nodes

---
+++ 222 Count Complete Tree Nodes/222count-complete-tree-nodes.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findhieght_left(TreeNode* root){
+        int hgt=0;
+        while(root){
+            hgt++;
+            root=root->left;
+        }
+        return hgt;
+    }
+
+    int findhieght_right(TreeNode* root){
+        int hgt=0;
+        while(root){
+            hgt++;
+            root=root->right;
+        }
+        return hgt;
+    }
+
+    int countNodes(TreeNode* root) {
+        if(root==NULL) return 0;
+        int lh=findhieght_left(root);
+        int rh=findhieght_right(root);
+        if(lh==rh) return ((1<<lh)-1);
+        return 1+countNodes(root->left)+countNodes(root->right);
+    }
+};
