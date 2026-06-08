e8eb5777953f5b818a7fe6a8cd896efe21a4e431
nikhilsinghal66 <nikhil24384@iiitd.ac.in>
2026-06-08 13:34:33 +0000

    Code challenge submission for question: 665 Non-decreasing Array

---
+++ 665 Non-decreasing Array/665non-decreasing-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int cnt = 0;
+        for(int i = 0; i < nums.size()-1; i++) {
+            if(nums[i] > nums[i+1]) {
+                cnt++;
+                if(cnt > 1)
+                    return false;
+                if(i == 0 || nums[i-1] <= nums[i+1]) {
+                    nums[i] = nums[i+1];
+                }
+                else {
+                    nums[i+1] = nums[i];
+                }
+            }
+        }
+        return true;
+    }
+};
