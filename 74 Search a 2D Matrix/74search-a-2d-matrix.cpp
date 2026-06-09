a21d6798e6997fa73ecbd68258f3ca25d33e9944
nikhilsinghal66 <nikhil24384@iiitd.ac.in>
2026-06-09 08:45:57 +0000

    Code challenge submission for question: 74 Search a 2D Matrix

---
+++ 74 Search a 2D Matrix/74search-a-2d-matrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+
+        for(int i=0;i<n;i++){
+            //binary search...
+            int l=0;
+            int r=m-1;
+            while(l<=r){
+                int mid=(l+r)/2;
+                if(target==matrix[i][mid]){
+                    return true;
+                }
+                if(target<matrix[i][mid]){
+                    r=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
+            }
+        }
+        return false;
+    }
+};
