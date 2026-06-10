0ecfefa58f0f6c524738789e00449ceaa3f8af39
nikhilsinghal66 <nikhil24384@iiitd.ac.in>
2026-06-10 09:01:00 +0000

    Code challenge submission for question: 1351 Count Negative Numbers in a Sorted Matrix

---
+++ 1351 Count Negative Numbers in a Sorted Matrix/1351count-negative-numbers-in-a-sorted-matrix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int row=0;
+        int col=m-1;
+        int count=0;
+        while(row<n && col>=0){
+            if(grid[row][col]<0){
+                count+=(n-row);
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return count;
+    }
+};
