5a7355f89835420ac757267efaf790b68d51eed1
Nikhil Singhal <nikhil24384@iiitd.ac.in>
2026-07-15 13:43:11 +0530

    Time: 0 ms (100%), Space: 10.3 MB (37.38%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        //Optimal Approach...
         int n=matrix.size();
-        vector<vector<int>> ans=matrix;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                matrix[j][n-i-1]=ans[i][j];
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
-        
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
     }
 };
