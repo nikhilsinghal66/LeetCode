0daf6a7a803f4ade2556c202623bda8fa3964b50
Nikhil Singhal <nikhil24384@iiitd.ac.in>
2026-07-15 12:33:35 +0530

    Time: 0 ms (100%), Space: 9.6 MB (63.81%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -10,12 +10,6 @@ public:
             }
             pascal.push_back(temp);
         }
-        if(numRows==1){
-            return pascal;
-        }
-        if(numRows==2){
-            return pascal;
-        }
         for(int i=2;i<numRows;i++){
             for(int j=1;j<i;j++){
                 pascal[i][j]=pascal[i-1][j]+pascal[i-1][j-1];
