12032105b52243059fc533064361a82a0deed555
Nikhil Singhal <nikhil24384@iiitd.ac.in>
2026-07-20 11:50:12 +0530

    Time: 0 ms (100%), Space: 18.2 MB (39.39%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
-    void f(vector<vector<int>>& temp,vector<vector<int>>& grid){
-        // i to m-1;
-        int n=grid.size();
-        int m=grid[0].size();
-        for(int i=1;i<m;i++){
-            for(int j=0;j<n;j++){
-                temp[j][i]=grid[j][i-1];
-            }
-        }
-        //gird[i][n-1] to grid[i+1][0]...
-        for(int i=1;i<n;i++){
-            temp[i][0]=grid[i-1][m-1];
-        }
-        //grid[m-1][n-1] to grid[0][0]...
-        temp[0][0]=grid[n-1][m-1];
-    }
-
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>> temp=grid;
-        while(k--){
-            f(temp,grid);
-            grid=temp;
+        k=k%(n*m);
+        vector<vector<int>> ans(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int ind=i*m+j;
+                int newInd=(ind+k)%(n*m);
+                ans[newInd/m][newInd%m]=grid[i][j];
+            }
         }
-        return temp; 
+        return ans;
     }
 };
