d7563fcb71c4c532d0f6c9febca19669cfd02429
Nikhil Singhal <nikhil24384@iiitd.ac.in>
2026-07-20 12:44:57 +0530

    Time: 12 ms (10.09%), Space: 15.3 MB (9.11%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void f(int ind,int n,vector<vector<string>> &ans,vector<vector<char>> &temp,unordered_map<int ,set<char>> &row_check,unordered_map<int ,set<char>> &col_check,unordered_map<int ,set<char>> &D_right,unordered_map<int ,set<char>> &D_left){
+    void f(int ind,int n,vector<vector<string>> &ans,vector<vector<char>> &temp,unordered_map<int ,set<char>> &col_check,unordered_map<int ,set<char>> &D_right,unordered_map<int ,set<char>> &D_left){
         if(ind==n){
             vector<string> var;
             for(int i=0;i<n;i++){
@@ -15,17 +15,15 @@ public:
             return;
         }
         for(int i=0;i<n;i++){
-            if(row_check[ind].count('Q') || col_check[i].count('Q') || D_right[ind+i].count('Q') || D_left[ind-i].count('Q') ){
+            if(col_check[i].count('Q') || D_right[ind+i].count('Q') || D_left[ind-i].count('Q') ){
                 continue;
             }
             temp[ind][i]='Q';
-            row_check[ind].insert('Q');
             col_check[i].insert('Q');
             D_right[ind+i].insert('Q');
             D_left[ind-i].insert('Q');
-            f(ind+1,n,ans,temp,row_check,col_check,D_right,D_left);
+            f(ind+1,n,ans,temp,col_check,D_right,D_left);
             temp[ind][i]='.';
-            row_check[ind].erase('Q');
             col_check[i].erase('Q');
             D_right[ind+i].erase('Q');
             D_left[ind-i].erase('Q');
@@ -35,12 +33,10 @@ public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<vector<char>> temp(n,vector<char>(n,'.'));
-        // vector<set<int>> row_check(n,set<char> (m));
-        unordered_map<int ,set<char>> row_check;
         unordered_map<int ,set<char>> col_check;
         unordered_map<int,set<char>> D_right;
         unordered_map<int,set<char>> D_left;
-        f(0,n,ans,temp,row_check,col_check,D_right,D_left);
+        f(0,n,ans,temp,col_check,D_right,D_left);
         return ans;
     }
 };
